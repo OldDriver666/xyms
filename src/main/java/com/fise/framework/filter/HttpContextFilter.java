617fb152ebd805a39b9a4c22e544c0d0e6c2d32d
chenzhongchao <chenzc@fise.com.cn>
2017-08-21 17:15:10 +0800

    跨域问题

--- src/main/java/com/fise/framework/filter/HttpContextFilter.java
+++ src/main/java/com/fise/framework/filter/HttpContextFilter.java
@@ -35,6 +35,7 @@ public class HttpContextFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse)servletResponse;
 		
 		if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+		    response.setStatus(200);
 			return ;
 		}
 		
