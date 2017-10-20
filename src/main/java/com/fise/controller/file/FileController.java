6607bc133f5c308e493bd5a7850098d2e6ff704c
chenzhongchao <chenzc@fise.com.cn>
2017-10-20 16:10:20 +0800

    配置 公网测试环境

--- src/main/java/com/fise/controller/file/FileController.java
+++ src/main/java/com/fise/controller/file/FileController.java
@@ -40,7 +40,7 @@ public class FileController {
             for(int i=0;i<uploadfile.length;i++){
                 file=uploadfile[i];
                 
-                String path="/home/fise/bin/www/upload";
+                String path="/home/fise/www/upload";
                 String filename=file.getOriginalFilename().replace(".", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())+".");
                 File dir=new File(path,filename);
                 if(!dir.exists()){
@@ -49,9 +49,9 @@ public class FileController {
             
                 file.transferTo(dir);
                 if(i==0){
-                    pictureURL="http://192.168.2.250:8888/upload"+"/"+filename;
+                    pictureURL="http://120.78.145.162:8080/upload"+"/"+filename;
                 }else {
-                    pictureURL=pictureURL+"http://192.168.2.250:8888/upload/"+filename;
+                    pictureURL=pictureURL+"http://120.78.145.162:8080/upload/"+filename;
                 }
         
             }
