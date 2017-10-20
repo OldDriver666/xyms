4533cd9a5b331369b5f13f1b8b1f26e69b64986e
liuzg <liuzg1@fise.com.cn>
2017-10-20 11:49:15 +0800

    外网服务器 小雨管理系统

--- html/manage/devManage/js/dev-info.js
+++ html/manage/devManage/js/dev-info.js
@@ -99,6 +99,7 @@ $(function() {
         loadDevTypeData : function() {
             var myDevTypeArray = JSON.parse(localStorage.getItem("myDevTypeArray"));
             $("#pageDevType").tmpl(myDevTypeArray).appendTo('#input-devType');
+            $("#pageDevType").tmpl(myDevTypeArray).appendTo('#input-devType2');
         },
 		//编辑数据
 		edit : function() {
@@ -137,7 +138,147 @@ $(function() {
                     }
 				});
 			}
-		}
+		},
+        //获取并处理批量插入的数据
+        getDevTxtInfo : function () {
+            var addCount = 10;
+            var txtDevInfoList = $(".fileContentTxt").text();
+            var txtDevType = $("#input-devType2").val();
+            var txtDevInfo_arr = txtDevInfoList.split(/\s+/);//文件里一共有多少条数据
+            var txt_IME_arr = [];
+            var txt_XW_arr = [];
+            for(i=0; i<txtDevInfo_arr.length; i++){
+                var txt_dev_item_arr = txtDevInfo_arr[i].split(",");
+                txt_IME_arr.push(txt_dev_item_arr[1]);//得到所有IME参数值的数组
+                txt_XW_arr.push(txt_dev_item_arr[3]);//得到所有XW参数值的数组
+            };
+            var add_data_count = Math.ceil(txtDevInfo_arr.length/addCount);//计算得到要通过执行几次插入
+            var add_data_last =  txtDevInfo_arr.length % addCount;//计算得到最后一次插入时，还剩几条数据
+
+            //每隔一定时间调用一次插入
+            var s = null;
+            var k = -1;
+            s = setInterval(function(){
+                k++;
+                var flag = null;
+                var n = k * addCount;
+                var num = 10;//每次插入几条数据的参数
+                if(k == add_data_count-1){
+                    var m = n + add_data_last;
+                    flag = 1;
+                    num = add_data_last;//如果执行到最后一次调用，最后一次剩下的数据条数赋给num,作为对应的参数值
+                    clearInterval(s);//执行完最后一次调用后关闭计时器
+                }else{
+                    var m = n + addCount;
+                }
+                var param_arr = [];
+                param_arr.splice(0, param_arr.length);//每执行完一次后清空下该数组
+                //遍历分批次插入时，每一次要插入的数据
+                for (l = n; l < m; l++) {
+                    var param = new Object();
+                    param.ime = txt_IME_arr[l];
+                    param.account = txt_XW_arr[l];
+                    param.depart_id = parseInt(companyId);
+                    param.type = parseInt(txtDevType);
+                    param_arr.push(param);
+                }
+                action.addDevFile(param_arr,flag,num)
+            },1000);
+
+        },
+        //批量添加设备信息
+        addDevFile : function (param_arr,flag,num) {
+            var url = ctx + "xiaoyusvr/boss/fisedevice/excel_import";
+            var data = {
+                UserName: userName,
+                AuthenticCode: token_value,
+                DeviceNo: num,//每次插入几条数据
+                DeviceInfo: param_arr
+            };
+            Util.ajaxLoadData(url,data,moduleId,"POST",true,function(result) {
+                if (result.code == ReturnCode.SUCCESS) {
+                    //通过函数传参得到flag,如果值为1时说明已经执行到最后一次调用，然后关闭弹窗，提示添加完成，获取最新数据
+                    if(parseInt(flag) == 1){
+                        $("#modal-loading").modal('hide');
+                        toastr.success("添加完成!");
+                        action.loadPageData();
+                        // action.getDevStatusData();
+                    }
+                }else if(result.code == 1){
+                    toastr.error(result.ErrorInfo);
+                    $("#modal-loading").modal('hide');
+                }else{
+                    alert("添加失败！");
+                    $("#modal-loading").modal('hide');
+                }
+            })
+        },
+        //批量添加设备信息
+        addDevInfoFile : function () {
+            var userid = Util.cookieStorage.getCookie("id");
+            var str =  "5|5|5|" + userid + "|" + moduleId;
+            var access_Token = Util.cookieStorage.getCookie("accessToken");
+
+            var form = document.getElementById('upload-form');
+            var formData = new FormData(form);
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', form.action);
+            /*xhr.setRequestHeader("Accept", "application/json");
+            xhr.setRequestHeader("FISE-UA", str);
+            xhr.setRequestHeader("FISE-AccessToken", access_Token);
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");*/
+            /*xhr.onload = function () {
+                if (xhr.status === 200) {
+                    $("#modal-loading").modal('hide');
+                    toastr.success("添加完成!");
+                    action.loadPageData();
+                } else {
+                    $("#modal-loading").modal('hide');
+                    toastr.success("出错了!");
+                }
+            };*/
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var resJson = JSON.parse(xhr.responseText)
+                    $("#modal-loading").modal('hide');
+                    toastr.success("添加完成!");
+                    action.loadPageData();
+                } else {
+                    $("#modal-loading").modal('hide');
+                    toastr.success("出错了!");
+                }
+            };
+            xhr.send(formData);
+
+            /*var getXmlHttpRequest = function() {
+                if (window.XMLHttpRequest) {
+                    return new XMLHttpRequest();
+                } else if (window.ActiveXObject) {
+                    return new ActiveXObject("Microsoft.XMLHttpRequest");
+                }
+            };
+            var xhr = getXmlHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var resJson = JSON.parse(xhr.responseText)
+                    //cb(resJson);
+                }
+            };
+            xhr.open("post", $(this).attr("http://192.168.2.250:8787/xiaoyusvr/boss/fisedevice/excel_import"), true);
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.setRequestHeader("FISE-UA", str);
+            xhr.setRequestHeader("FISE-AccessToken", access_Token);
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+           /!* var dataStr = '';
+            for (var i in data) {
+                if (dataStr) {
+                    dataStr += '&';
+                }
+                dataStr += i + '=' + data[i];
+            }*!/
+            xhr.send(formData);*/
+        }
 	};
 	window.action = action;
     action.init();
@@ -254,7 +395,7 @@ $(function() {
         if($("#filepath").val() != '' && $("#input-devType2").val() != ''){
             $("#addTempl-modal2").modal('hide');
             $("#modal-loading").modal({backdrop: 'static', keyboard: false, show: true});
-            action.getDevTxtInfo();
+            action.addDevInfoFile();
         }else if($("#filepath").val() == ''){
             alert("请选择文件！");
         }else if($("#input-devType2").val() == ''){
