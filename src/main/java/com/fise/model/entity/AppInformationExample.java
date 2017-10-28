334299465d2e5bac4ad6c144cdcfc48d83b9db72
tan <jayi1314@163.com>
2017-10-28 10:08:06 +0800

    修改app_name查询的bug

--- src/main/java/com/fise/model/entity/AppInformationExample.java
+++ src/main/java/com/fise/model/entity/AppInformationExample.java
@@ -295,7 +295,7 @@ public class AppInformationExample {
         }
 
         public Criteria andAppNameLike(String value) {
-            addCriterion("CONCAT( app_name , app_spell )", value, "appName");
+            addCriterion("CONCAT( app_name , app_spell ) like", value, "appName");
             return (Criteria) this;
         }
 
