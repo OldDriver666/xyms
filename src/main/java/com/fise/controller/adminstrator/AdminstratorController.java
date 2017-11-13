c65657274a1af5c547762b86f441aac8cdf8f7a2
zn-xwsy012 <wuj@fise.com.cn>
2017-11-13 09:17:40 +0800

    app发生修改后，将其状态设置为待审核。

--- src/main/java/com/fise/controller/adminstrator/AdminstratorController.java
+++ src/main/java/com/fise/controller/adminstrator/AdminstratorController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.dataformat.yaml.snakeyaml.emitter.Emitable;
 import com.fise.base.ErrorCode;
 import com.fise.base.Response;
 import com.fise.framework.annotation.IgnoreAuth;
