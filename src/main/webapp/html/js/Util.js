e1e86cc62c86d0754d1d1cf5757fad59ec1133bd
liuzg <liuzg1@fise.com.cn>
2017-12-02 11:37:13 +0800

    网络更新fise-wi

--- src/main/webapp/html/js/Util.js
+++ src/main/webapp/html/js/Util.js
@@ -797,8 +797,8 @@ Util.regionArgumentsDetail = function(regionlist){
 
 //获取当前域名
 Util.pathName = function(){
-    ctx = "http://192.168.2.250:8787/";        //test version
-	//ctx = "http://120.78.145.162:8787/";        //test version
+    //ctx = "http://192.168.2.250:8787/";        //test version
+	ctx = "http://xiaoyu.fise-wi.com:8787/";        //test version
    // ctx = "http://192.168.99.11:8080/";        //non-stop server version
      /*ctx = "http://192.168.2.196:8610/";*/
     /*ctx = "http://bossdev.wn517.com/";*/
