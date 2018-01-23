0d865f33a41d275564b14441da671a23d47ac877
chenzhongchao <chenzc@fise.com.cn>
2018-01-23 11:15:55 +0800

    修正

--- src/main/java/com/fise/service/administrator/impl/DeveloperServiceImpl.java
+++ src/main/java/com/fise/service/administrator/impl/DeveloperServiceImpl.java
@@ -143,15 +143,15 @@ public class DeveloperServiceImpl implements IDeveloperService {
 		WiAdmin dev = adminDao.selectByPrimaryKey(developer.getId());
 		try {
 			HtmlEmail email = new HtmlEmail();// 不用更改
-			email.setHostName("smtp.qq.com");// 需要修改，126邮箱为smtp.126.com,163邮箱为163.smtp.com，QQ为smtp.qq.com
+			email.setHostName("smtp.exmail.qq.com");// 需要修改，126邮箱为smtp.126.com,163邮箱为163.smtp.com，QQ为smtp.qq.com
 			email.setCharset("UTF-8");
 			email.setSSLOnConnect(true);
 
 			email.addTo(dev.getEmail());// 收件地址
 			//这是本人自己qq邮箱，测试使用，建议后期修改邮箱地址
-			email.setFrom("2839117863@qq.com", "天堂遗孤");// 此处填邮箱地址和用户名,用户名可以任意填写
+			email.setFrom("chenzc@fise.com.cn", "fise智能");// 此处填邮箱地址和用户名,用户名可以任意填写
 			//修改新的客户端授权码
-			email.setAuthentication("2839117863@qq.com", "vjulajvpgeiqdcjd");// 此处填写邮箱地址和客户端授权码
+			email.setAuthentication("chenzc@fise.com.cn", "Fise321");// 此处填写邮箱地址和客户端授权码
 
 			email.setSubject("孙大大通讯");// 此处填写邮件名，邮件名可任意填写
 			if (dev.getStatus() == 0) {
