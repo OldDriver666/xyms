5b7cefa23f309795371b46fa82ec1acb9633af89
liuzg <liuzg1@fise.com.cn>
2018-01-29 10:38:44 +0800

    后台管理系统/开发者注册/忘记密码----test环境

--- src/main/webapp/html/manage/appstore/js/app-info-rd-manage.js
+++ src/main/webapp/html/manage/appstore/js/app-info-rd-manage.js
@@ -972,13 +972,26 @@ function imgShow(outerdiv, innerdiv, bigimg, _this){
 
 
 function getBase64Image(img) {
+    /*var image = new Image()
+    image.src = img.src
+    image.onload = function () {
+        var expectWidth = this.naturalWidth
+        var expectHeight = this.naturalHeight
+        var canvas = document.createElement("canvas");
+        canvas.width = expectWidth;
+        canvas.height = expectHeight;
+        var ctxx = canvas.getContext("2d");
+        ctxx.drawImage(img, 0, 0, expectWidth, expectHeight);
+        var dataURL = canvas.toDataURL("image/jpg");
+        return dataURL
+    }*/
     var canvas = document.createElement("canvas");
     canvas.width = img.width;
     canvas.height = img.height;
     var ctxx = canvas.getContext("2d");
     ctxx.drawImage(img, 0, 0, img.width, img.height);
-    var dataURL = canvas.toDataURL("image/png");
-    return dataURL // return dataURL.replace("data:image/png;base64,", "");
+    var dataURL = canvas.toDataURL("image/jpg");
+    return dataURL
 }
 
 
