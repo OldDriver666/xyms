489938f5e7067a5405d47aa91eb300ab11cd11bc
liuzg <liuzg1@fise.com.cn>
2018-03-29 17:35:34 +0800

    更改小雨问答圈index.html样式

--- src/main/webapp/html/js/index.js
+++ src/main/webapp/html/js/index.js
@@ -5,8 +5,83 @@ $(function(){
 	var id = Util.cookieStorage.getCookie("id");
     var nickName = Util.cookieStorage.getCookie("nickName");
 
+    //点击一级菜单
+    $('#menuContent').on('click', '.tpl-left-nav-link-list', function () {
+        $(this).next().toggle(200);
+        $(this).parent().siblings().find('.tpl-left-nav-sub-menu').slideUp(200);
+        var $this = $(this);
+        var url = $this.data('url');
+        if (url !== "" && url !== "data-insert=1") {
+            $("#menuName").html('');
+            var menuid = $this.data('menuid');
+            var insertRight = $this.data('insert');
+            var queryRight = $this.data('query');
+            var updateRight = $this.data('update');
+            var moduleName = $this.data('name');
+            var nameArray = [{name: moduleName}]
+            url += "?insertAuth=" +(insertRight) +"&queryAuth=" +(queryRight) +"&updateAuth=" +(updateRight)+"&moduleId=" +(menuid);
+            $('#iframePage').attr('src', url);
+            $("#showMenuName").tmpl(nameArray).appendTo('#menuName');
+        }
+    });
+
+    //点击二级菜单
+    $('.tpl-left-nav').on('click', '.childrenMenu', function () {
+        $("#menuName").html('');
+        var $this = $(this);
+        var url = $this.data('url');
+        var menuid = $this.data('menuid');
+        var insertRight = $this.data('insert');
+        var queryRight = $this.data('query');
+        var updateRight = $this.data('update');
+        var moduleName = $this.data('name');
+        var nameArray = [{name: moduleName}]
+        url += "?insertAuth=" +(insertRight) +"&queryAuth=" +(queryRight) +"&updateAuth=" +(updateRight)+"&moduleId=" +(menuid);
+        if (url !== undefined) {
+            $('#iframePage').attr('src', url);
+            $("#showMenuName").tmpl(nameArray).appendTo('#menuName');
+        }
+        $('#menuDowns').html('');
+    });
+
+    //模糊搜索菜单栏功能
+    $("#menuSearch").bind('click',function(){
+        if($("#menuInput").val() == ''){
+            return
+        }
+        $('#menuDowns').html("");
+        var url = ctx + "xiaoyusvr/boss/role/queryAuthByName";
+        var data = {
+            "role_id":parseInt(roleId),
+            "company_id":parseInt(companyId),
+            "name": $("#menuInput").val()
+        };
+        var moduleId= 0;
+        Util.ajaxLoadData(url,data,moduleId,"POST",true,function(result) {
+            if(result.code == ReturnCode.SUCCESS){
+                $("#searchMenu").tmpl(result.data).appendTo('#menuDowns');
+            }  else {
+                toastr.error(result.msg);
+            }
+        },function() {
+            toastr.error("服务器开个小差，请稍后重试！");
+        });
+    });
+
+    //个人信息
+    $(".personal-Info").on('click', function(){
+        var $this = $(this);
+        var url = $this.data('url');
+        $("#menuName").html('');
+        var moduleName = $this.data('name');
+        var nameArray = [{name: moduleName}]
+        $('#iframePage').attr('src', url);
+        $("#showMenuName").tmpl(nameArray).appendTo('#menuName');
+        /*$('#showPerInfo').toggle();*/
+    });
+
     //安全退出
-    $("#header-safeExit").on('click', function(){
+    $(".header-safeExit").on('click', function(){
         var url = ctx + "xiaoyusvr/boss/admin/logout";
         var data = {"admin_id": parseInt(id)};
         Util.ajaxLoadData(url,data,0,"POST",true,function(result) {
@@ -23,13 +98,11 @@ $(function(){
                 Util.cookieStorage.clearCookie("userType");
                 localStorage.clear();
                 window.location.href = "login.html";
-            } else if(result.Status == 1){
-                alert("服务器开个小差，请稍后重试！");
-            } else {
-                alert("账户名、密码或错误！");
+            }  else {
+                toastr.error("账户名、密码或错误！");
             }
         },function() {
-            alert("服务器开个小差，请稍后重试！");
+            toastr.error("服务器开个小差，请稍后重试！");
         });
     });
 
@@ -74,13 +147,11 @@ $(function(){
                         $("#pageMenu").tmpl(parent_data[i]).appendTo('#menuContent');
                     }
 
-                } else if(result.Status == 1){
-                    alert("服务器开个小差，请稍后重试！");
-                } else {
-                    //alert("账户名、密码或错误！");
+                }  else {
+                    toastr.error(result.msg);
                 }
             },function() {
-                alert("服务器开个小差，请稍后重试！");
+                toastr.error("服务器开个小差，请稍后重试！");
             });
 		},
         sortBy: function(attr,rev){
