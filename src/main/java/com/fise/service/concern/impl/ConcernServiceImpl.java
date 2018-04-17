6acef4ce8210d6b31c6c0f372dc83121ba649f94
tany <jayi1314@163.com>
2018-04-17 14:25:46 +0800

    关注查询增加用户昵称和问题标题查询条件

--- src/main/java/com/fise/service/concern/impl/ConcernServiceImpl.java
+++ src/main/java/com/fise/service/concern/impl/ConcernServiceImpl.java
@@ -398,6 +398,13 @@ public class ConcernServiceImpl implements IConcernService{
         if(page.getParam().getProblemId()!=null){
             criteria.andProblemIdEqualTo(page.getParam().getProblemId());
         }
+        if(!StringUtil.isEmpty(page.getParam().getNick())){
+            page.getParam().setNick("%" + page.getParam().getNick() + "%");
+            criteria.andNickLike(page.getParam().getNick());
+        }
+        if(StringUtil.isNotEmpty(page.getParam().getTitle())){
+            criteria.andTitleLike("%" + page.getParam().getTitle() + "%");
+        }
         
         List<Concern> list=concernDao.selectByPage(example, page);
         
