57c44bd139dd5abdf632c5c78c51c4f4c4bb24e0
liuzg <liuzg1@fise.com.cn>
2018-06-14 11:22:13 +0800

    设备头像.html---查看模式下直接显示图片

--- src/main/webapp/html/manage/sysManage/js/devAvatar-manage.js
+++ src/main/webapp/html/manage/sysManage/js/devAvatar-manage.js
@@ -371,17 +371,38 @@ $(function() {
     //编辑获取数据
     $("#pageContent").on("click",".table-edit-btn",function(){
         var that = $(this).parent().parent();
-        var imgUrl = that.find("td").eq(4).find("img").prop("src");
+        /*var imgUrl = that.find("td").eq(4).find("img").prop("src");
         var arrObj = imgUrl.split("//");
         var startIndx = arrObj[1].indexOf("/");
-        var relUrl = arrObj[1].substring(startIndx).slice(1);
+        var relUrl = arrObj[1].substring(startIndx).slice(1);*/
+        $("#iconShow").empty();
+        var iconList = $.trim(that.find("td").eq(4).find("img").prop("src")).split(";");
+        var myDiv1 = document.getElementById("iconShow");
+        for(var i=0; i < iconList.length; i++){
+            var img1 = document.createElement("img");
+            img1.setAttribute("class", "newIcon");
+            img1.width =70;
+            img1.src = iconList[i];
+            img1.onclick=function() {
+                var _this = $(this);//将当前的pimg元素作为_this传入函数
+                imgShow("#outerdiv", "#innerdiv", "#bigimg", _this);
+                /*if(this.width == 420){
+                 this.width+=200;
+                 } else {
+                 this.width = 420;
+                 }*/
+                /*this.style.zoom= 2;
+                 window.open(this.src);*/
+            };
+            myDiv1.appendChild(img1);
+        }
 
         $("#input-config_id").val(that.find("td").eq(0).text());
         $("#input-depart_id-txt").val(that.find("td").eq(2).text());
         $("#input-depart_idNo").val(that.find("td").eq(1).text());
         $("#input-devType-txt").val(that.find("td").eq(3).text());
         $("#input-devTypeNo").val(that.find("td").eq(5).text());
-        $("#input-avatar").val(relUrl);
+        /*$("#input-avatar").val(imgUrl);*/
         $("#addTempl-modal").modal("show");
     });
 
@@ -426,6 +447,45 @@ $(function() {
     });
 
 });
+
+function imgShow(outerdiv, innerdiv, bigimg, _this){
+    var src = _this.attr("src");//获取当前点击的pimg元素中的src属性
+    $(bigimg).attr("src", src);//设置#bigimg元素的src属性
+
+    /*获取当前点击图片的真实大小，并显示弹出层及大图*/
+    $("<img/>").attr("src", src).load(function(){
+        var windowW = $(window).width();//获取当前窗口宽度
+        var windowH = $(window).height();//获取当前窗口高度
+        var realWidth = this.width;//获取图片真实宽度
+        var realHeight = this.height;//获取图片真实高度
+        var imgWidth, imgHeight;
+        var scale = 0.8;//缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放
+
+        if(realHeight>windowH*scale) {//判断图片高度
+            imgHeight = windowH*scale;//如大于窗口高度，图片高度进行缩放
+            imgWidth = imgHeight/realHeight*realWidth;//等比例缩放宽度
+            if(imgWidth>windowW*scale) {//如宽度扔大于窗口宽度
+                imgWidth = windowW*scale;//再对宽度进行缩放
+            }
+        } else if(realWidth>windowW*scale) {//如图片高度合适，判断图片宽度
+            imgWidth = windowW*scale;//如大于窗口宽度，图片宽度进行缩放
+            imgHeight = imgWidth/realWidth*realHeight;//等比例缩放高度
+        } else {//如果图片真实高度和宽度都符合要求，高宽不变
+            imgWidth = realWidth;
+            imgHeight = realHeight;
+        }
+        $(bigimg).css("width",imgWidth);//以最终的宽度对图片缩放
+
+        var w = (windowW-imgWidth)/2;//计算图片与窗口左边距
+        var h = (windowH-imgHeight)/2;//计算图片与窗口上边距
+        $(innerdiv).css({"top":h, "left":w});//设置#innerdiv的top和left属性
+        $(outerdiv).fadeIn("fast");//淡入显示#outerdiv及.pimg
+    });
+
+    $(outerdiv).click(function(){//再次点击淡出消失弹出层
+        $(this).fadeOut("fast");
+    });
+}
 
 function getBase64Image(img) {
     var canvas = document.createElement("canvas");
